3c5fcd05ae18394cd936f8d96f71b5bb662276f9
Mornevanzyl <catbeatsadrum@gmail.com>
2021-03-10 17:09:51 +0000

    Added numerical input validation for Transaction class. Updated unit tests

--- spec/TransactionSpec.js
+++ spec/TransactionSpec.js
@@ -17,4 +17,12 @@ describe('Transaction', () => {
   it('has a transaction date', () => {
     expect(transaction.date()).toEqual('01/01/2020');
   });
+
+  it('accepts only valid numerics for transaction values', () => {
+    expect(() => { new Transaction('ten'); }).toThrowError('Invalid input: Only valid numbers are accepted');
+  });
+
+  it('accepts values in strings/integer/float format with a maximum of 2 decimal places', () => {
+    expect(() => { new Transaction(10.001); }).toThrowError('Invalid input: Only numbers with maximum 2 decimal places are allowed');
+  });
 });
--- src/Transaction.js
+++ src/Transaction.js
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 class Transaction {
   constructor(value, date = this.formatDate(new Date())) {
-    this._value = value;
+    this._value = this.validateInput(value);
     this._date = date;
   }
 
@@ -17,5 +17,24 @@ class Transaction {
 
   formatDate(date) {
     return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear().toString()}`;
+  }
+
+  validateInput(value, errorPostFix) {
+    const exp = /^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/;
+
+    value = parseFloat(value); // Convert valid numbers in string format to float
+
+    if (Number.isNaN(value)) {
+      throw new Error('Invalid input: Only valid numbers are accepted');
+    }
+
+    if (!exp.test(value)) {
+      throw new Error('Invalid input: Only numbers with maximum 2 decimal places are allowed');
+    }
+
+    if (!(value > 0)) {
+      throw new Error(`Only positive values are accepted for ${errorPostFix}`);
+    }
+    return value;
   }
 }
