effefda96a3f62a86b95570077ad93c403f3dcca
Mornevanzyl <catbeatsadrum@gmail.com>
2021-03-10 08:05:19 +0000

    Added unit tests and functionality to validate input values for deposits and withdrawals

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -11,24 +11,43 @@ describe('Account', () => {
     it('is an instance of the Account class', () => {
       expect(account).toBeInstanceOf(Account);
     });
+
     it('has an account balance of 0 when opened', () => {
       expect(account.balance()).toEqual(0);
     });
+
     it('keeps a transaction record of all deposts and withdrawals', () => {
       account.deposit(10);
       account.withdraw(5);
       expect(account.transactions().length).toEqual(2);
     });
+
+    it('can output an account statement on request', () => {
+      account.deposit(1000);
+    });
   });
 
   describe('Deposits:', () => {
+    it('accepts only valid numerics for deposit amounts', () => {
+      expect(() => { account.deposit('ten'); }).toThrowError('Invalid input: Only valid numbers are accepted');
+    });
+
+    it('accepts both numbers and strings with a maximum of 2 decimal places', () => {
+      expect(() => { account.deposit(10.001); }).toThrowError('Invalid input: Only numbers with maximum 2 decimal places are allowed');
+    });
+
     it('has the facility to deposit money into the account', () => {
       account.deposit(10);
       expect(account.balance()).toEqual(10);
     });
 
     it('only accepts positive amounts for deposits', () => {
-      expect(() => { account.deposit(-10); }).toThrowError('Only positive amounts are accepted for deposits');
+      expect(() => { account.deposit(-10); }).toThrowError('Only positive values are accepted for deposits');
+    });
+
+    it('can accept an optional transaction date', () => {
+      account.deposit(1000, '10/01/2012');
+      expect(account.transactions()[0][0]).toEqual('10/01/2012');
     });
   });
 
@@ -45,6 +64,11 @@ describe('Account', () => {
 
     it('validates sufficient funds in the account for the requested withdrawal', () => {
       expect(() => { account.withdraw(10); }).toThrowError('Insufficient funds in account for requested withdrawal');
+    });
+
+    it('can accept an optional transaction date', () => {
+      account.deposit(500, '14/01/2012');
+      expect(account.transactions()[0][0]).toEqual('14/01/2012');
     });
   });
 
--- src/Account.js
+++ src/Account.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable padded-blocks */
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
 // eslint-disable-next-line no-unused-vars
 class Account {
@@ -10,27 +13,42 @@ class Account {
     return this._balance;
   }
 
-  deposit(amount, date = new Date()) {
-    if (!(amount > 0)) {
-      throw new Error('Only positive amounts are accepted for deposits');
-    }
-    this._transactions.push([this.formatDate(date), amount]);
+  deposit(amount, date = this.formatDate(new Date())) {
+
+    amount = this.validateInput(amount, 'deposits');
+
     this._balance += amount;
+    this._transactions.push([date, amount, this._balance]);
   }
 
-  withdraw(amount, date = new Date()) {
-    if (!(amount > 0)) {
-      throw new Error('Only positive values are accepted for withdrawals');
-    }
+  withdraw(amount, date = this.formatDate(new Date())) {
+
+    amount = this.validateInput(amount, 'withdrawals');
+
     if (this.hasInsuffientFunds(amount)) {
       throw new Error('Insufficient funds in account for requested withdrawal');
     }
-    this._transactions.push([this.formatDate(date), -amount]);
     this._balance -= amount;
+    this._transactions.push([date, -amount, this._balance]);
+  }
+
+  printAccountStatement() {
+    let outputString = 'date || credit || debit || balance\n';
+    this.reverseTransactions().forEach((trn) => {
+      outputString += `${trn[0]} || ${trn[1] > 0 ? trn[1].toFixed(2) : ''} || ${trn[1] < 0 ? -trn[1].toFixed(2) : ''} || ${trn[2].toFixed(2)}\n`;
+    });
+    // eslint-disable-next-line no-console
+    console.log(outputString);
+    return outputString;
   }
 
   transactions() {
     return this._transactions;
+  }
+
+  reverseTransactions() {
+    const transactions = [...this.transactions()];
+    return transactions.reverse();
   }
 
   hasInsuffientFunds(amount) {
@@ -39,5 +57,24 @@ class Account {
 
   formatDate(date) {
     return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear().toString()}`;
+  }
+
+  validateInput(value, errorPostFix) {
+    const exp = /^\s*-?[1-9]\d*(\.\d{1,2})?\s*$/;
+
+    value = parseFloat(value); // Convert valid numbers in string format to float
+
+    if (Number.isNaN(value)) {
+      throw new Error('Invalid input: Only valid numbers are accepted');
+    }
+
+    if (!exp.test(value)) {
+      throw new Error('Invalid input: Only numbers with maximum 2 decimal places are allowed');
+    }
+
+    if (!(value > 0)) {
+      throw new Error(`Only positive values are accepted for ${errorPostFix}`);
+    }
+    return value;
   }
 }
